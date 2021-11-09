1df28028447d203b5752bf2f3d3725a80da1585f
Guilherme <guilhaugusto05@gmail.com>
2021-11-09 20:59:11 -0300

    Exercício Bônus

--- src/components/DogContainer.jsx
+++ src/components/DogContainer.jsx
@@ -9,13 +9,24 @@ class DogContainer extends Component {
     this.state = {
       loading: true,
       dogObj: undefined,
+      dogName: '',
     };
 
     this.requestPhoto = this.requestPhoto.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.saveDog = this.saveDog.bind(this);
+    this.loadSavedDog = this.loadSavedDog.bind(this);
   }
 
   componentDidMount() {
-    this.requestPhoto();
+    const dogInfo = localStorage.getItem('dogInfo');
+
+    if (dogInfo) {
+      const { photo } = JSON.parse(dogInfo);
+      this.loadSavedDog(photo);
+    } else {
+      this.requestPhoto();
+    }
   }
 
   shouldComponentUpdate() {
@@ -25,7 +36,7 @@ class DogContainer extends Component {
   }
 
   componentDidUpdate(_prevProps, prevState) {
-    const { dogObj: { message = '' } = {} } = this.state;
+    const { dogObj: { message = '' } = {}, dogArray } = this.state;
     const { dogObj: { message: prevMsg = '' } = {} } = prevState;
 
     const dogType = message.split('/')[4];
@@ -34,7 +45,15 @@ class DogContainer extends Component {
     // eslint-disable-next-line no-alert
     if (dogType && dogType !== prevDogType) alert(`Raça: ${dogType}`);
 
-    sessionStorage.setItem('photoUrl', message);
+    if (dogArray !== undefined) {
+      localStorage.setItem('dogInfo', JSON.stringify(dogArray));
+    }
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState({
+      [name]: value,
+    });
   }
 
   requestPhoto() {
@@ -52,16 +71,40 @@ class DogContainer extends Component {
     });
   }
 
+  saveDog() {
+    const { dogName, dogObj: { message } } = this.state;
+    const dogInfo = { name: dogName, photo: message };
+
+    this.setState({ dogArray: dogInfo, dogName: '' });
+  }
+
+  loadSavedDog(imageUrl) {
+    this.setState({
+      dogObj: { message: imageUrl },
+      loading: false,
+    });
+  }
+
   render() {
-    const { requestPhoto } = this;
-    const { loading, dogObj: { message } = '' } = this.state;
+    const { requestPhoto, handleChange, saveDog } = this;
+    const { loading, dogObj: { message } = '', dogName } = this.state;
 
     if (loading) return <div className="loading"><LoaderSpin /></div>;
 
     return (
       <>
-        <div className="button-container">
+        <div className="container">
           <button type="button" onClick={ requestPhoto }>Au!</button>
+          <div className="input-container">
+            <input
+              type="text"
+              name="dogName"
+              placeholder="Dê um nome para este doguinho"
+              value={ dogName }
+              onChange={ handleChange }
+            />
+            <button type="button" onClick={ saveDog }>Guardar</button>
+          </div>
         </div>
         <div className="dog-photo">
           <DogPhoto source={ message } />
